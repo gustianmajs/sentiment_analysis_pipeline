78188052a1ca158c65be6ebb68932658b129fd2c
Gustian <125520997+gustianmajs@users.noreply.github.com>
2024-06-27 00:20:14 +0700

    update

--- plugins/sentiment_analysis_loader.py
+++ plugins/sentiment_analysis_loader.py
@@ -4,18 +4,19 @@ from sentiment_analysis import SentimentAnalysis
 import postgres_loader
 
 
-db = mongodb_loader.get('news', 'finnhub_news')
+def run_analysis():
+    db = mongodb_loader.get('news', 'finnhub_news')
 
-news = [x for x in db.finnhub_news.find()]
+    news = [x for x in db.finnhub_news.find()]
 
-output = []
-for news_summary in news:
-    output.append(SentimentAnalysis(text=news_summary["summary"]).execute())
+    output = []
+    for news_summary in news:
+        output.append(SentimentAnalysis(text=news_summary["summary"]).execute())
 
-    print(f"Summary: {news_summary['summary']} successfully analyzed.")
+        print(f"Summary: {news_summary['summary']} successfully analyzed.")
 
-sentiment_output =pd.DataFrame(output)
+    sentiment_output =pd.DataFrame(output)
 
-postgres_loader.load(sentiment_output, 'sentiment_news_analysis')
+    postgres_loader.load(sentiment_output, 'sentiment_news_analysis')
 
-print("Successfully loaded data into Postgres.")
+    print("Successfully loaded data into Postgres.")
